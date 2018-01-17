0d4af1ccffabe2f9ec1a38d94bf8c42825497fcc
Muhammad Ilyas <muhammad.ilyas@outlook.com>
2018-01-17 16:09:33 +0700

    no message

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -3,6 +3,5 @@
     <div class="clearfix"></div>
     <footer><a href="http://www.jasamarga.com" title="School of graphic &amp; web design" target="_blank">Jasa Marga Jagorawi</a></footer>
 </section>
-ilyas
 </body>
 </html>
